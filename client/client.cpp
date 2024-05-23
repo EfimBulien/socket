0ab0cddb43518563f847a5b46892d06f35ff1601
Ефим Бульен <143902970+EfimBulien@users.noreply.github.com>
2024-05-23 21:48:05 +0300

    ok

---
+++ client/client.cpp
@@ -0,0 +1,128 @@
+﻿#define WIN32_LEAN_AND_MEAN
+
+#include <iostream>
+#include <Windows.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+using namespace std;
+
+int main()
+{
+    WSADATA wsaData; // Эта структура содержит информацию о реализации Windows Sockets
+    ADDRINFO* addResult; // Этот указатель будет хранить результаты адресной информации
+    ADDRINFO hints; // Эта структура будет хранить подсказки для функции getaddrinfo
+    SOCKET ConnectSocket = INVALID_SOCKET; // Это будет наш сокет для подключения к серверу
+    const char* sendBuffer = "Hello from Client"; // Первое сообщение для отправки на сервер
+    const char* secondMessage = "Second message from Client"; // Второе сообщение для отправки на сервер
+    char recvBuffer[512]; // Буфер для хранения полученных данных от сервера
+
+    // Инициализация Winsock
+    int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (result != 0)
+    {
+        std::cout << "WSAStartup failes result" << std::endl;
+        return 1;
+    }
+
+    // Настраиваем структуру hints, чтобы указать тип соединения, который нам нужен
+    ZeroMemory(&hints, sizeof(hints)); // Обнуляем память для hints
+    hints.ai_family = AF_INET; // Используем IPv4
+    hints.ai_socktype = SOCK_STREAM; // Тип сокета потоковый (TCP)
+    hints.ai_protocol = IPPROTO_TCP; // Протокол TCP
+
+    // Определяем адрес и порт сервера
+    result = getaddrinfo("localhost", "666", &hints, &addResult);
+    if (result != 0)
+    {
+        std::cout << "Getaddrinfo wit error" << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    // Создаем сокет для подключения к серверу
+    ConnectSocket = socket(addResult->ai_family, addResult->ai_socktype, addResult->ai_protocol);
+    if (ConnectSocket == INVALID_SOCKET) {
+        std::cout << "Socket creation with " << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    // Подключаемся к серверу
+    result = connect(ConnectSocket, addResult->ai_addr, (int)addResult->ai_addrlen);
+    if (result == SOCKET_ERROR)
+    {
+        std::cout << "Unable to connect to server" << std::endl;
+        closesocket(ConnectSocket); // Закрываем сокет
+        freeaddrinfo(addResult); // Освобождаем адресную информацию
+        WSACleanup();
+        return 1;
+    }
+
+    // Отправляем первое сообщение на сервер
+    result = send(ConnectSocket, sendBuffer, (int)strlen(sendBuffer), MSG_DONTROUTE);
+
+    if (result == SOCKET_ERROR) 
+    {
+        std::cout << "Send failed with error" << std::endl;
+        // Очищаем и закрываем сокет
+        closesocket(ConnectSocket);
+        freeaddrinfo(addResult);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "First message sent successfully" << std::endl;
+
+    // Отправляем второе сообщение на сервер
+    result = send(ConnectSocket, secondMessage, (int)strlen(secondMessage), MSG_DONTROUTE);
+
+    if (result == SOCKET_ERROR) 
+    {
+        std::cout << "Send failed with error" << std::endl;
+        // Очищаем и закрываем сокет
+        closesocket(ConnectSocket);
+        freeaddrinfo(addResult);
+        WSACleanup(); //очситка WSAStartup
+        return 1;
+    }
+
+    std::cout << "Second message sent successfully" << std::endl;
+
+    // Получаем данные от сервера до тех пор, пока сервер не закроет соединение
+    do
+    {
+        ZeroMemory(recvBuffer, 512); // Обнуляем буфер приема
+        result = recv(ConnectSocket, recvBuffer, 512, 0); // Получаем данные от сервера
+        if (result > 0)
+        {
+            cout << "Received " << result << "bytes" << endl;
+            cout << "Received data " << recvBuffer << endl;
+        }
+        else if (result == 0)
+        {
+            cout << "Connection closed" << endl; // Соединение закрыто сервером
+        }
+        else
+        {
+            cout << "Received failed " << endl;
+            closesocket(ConnectSocket); // Закрываем сокет при ошибке
+        }
+    } while (result > 0);
+
+    // Завершаем соединение для отправки, так как больше данных отправлять не будем
+    result = shutdown(ConnectSocket, SD_SEND);
+    if (result == SOCKET_ERROR)
+    {
+        cout << "shutdown error " << endl;
+        freeaddrinfo(addResult);
+        WSACleanup(); //очситка WSAStartup
+        return 1;
+    }
+
+    // Очищаем и закрываем сокет
+    closesocket(ConnectSocket);
+    freeaddrinfo(addResult);
+    WSACleanup(); //очситка WSAStartup
+    return 0;
+}
---
+++ server/server.cpp
@@ -0,0 +1,134 @@
+﻿#define WIN32_LEAN_AND_MEAN
+
+#include <iostream>
+#include <Windows.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+int main()
+{
+    WSADATA wsaData;
+    ADDRINFO* addResult = nullptr;
+    ADDRINFO hints;
+    SOCKET ClientSocket = INVALID_SOCKET;
+    SOCKET ListenSocket = INVALID_SOCKET;
+
+    const char* sendBuffer = "Hello from server";
+    char recvBuffer[512];
+
+    int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+    if (result != 0) 
+    {
+        std::cout << "WSAStartup failed with result " << result << std::endl;
+        return 1;
+    }
+
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags = AI_PASSIVE;
+
+    result = getaddrinfo(NULL, "666", &hints, &addResult);
+
+    if (result != 0) 
+    {
+        std::cout << "Getaddrinfo failed with error " << result << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    ListenSocket = socket(addResult->ai_family, addResult->ai_socktype, addResult->ai_protocol);
+
+    if (ListenSocket == INVALID_SOCKET) 
+    {
+        std::cout << "Socket creation failed" << std::endl;
+        freeaddrinfo(addResult);
+        WSACleanup();
+        return 1;
+    }
+
+    result = bind(ListenSocket, addResult->ai_addr, (int)addResult->ai_addrlen);
+
+    if (result == SOCKET_ERROR) 
+    {
+        std::cout << "Binding socket failed" << std::endl;
+        closesocket(ListenSocket);
+        freeaddrinfo(addResult);
+        WSACleanup();
+        return 1;
+    }
+
+    result = listen(ListenSocket, SOMAXCONN);
+    if (result == SOCKET_ERROR) {
+        std::cout << "Listening on socket failed" << std::endl;
+        closesocket(ListenSocket);
+        freeaddrinfo(addResult);
+        WSACleanup();
+        return 1;
+    }
+
+    ClientSocket = accept(ListenSocket, NULL, NULL);
+
+    if (ClientSocket == INVALID_SOCKET) 
+    {
+        std::cout << "Accepting connection failed" << std::endl;
+        closesocket(ListenSocket);
+        freeaddrinfo(addResult);
+        WSACleanup();
+        return 1;
+    }
+
+    closesocket(ListenSocket);
+
+    do 
+    {
+        ZeroMemory(recvBuffer, 512);
+        result = recv(ClientSocket, recvBuffer, 512, 0);
+        if (result > 0) 
+        {
+            std::cout << "Received " << result << " bytes" << std::endl;
+            std::cout << "Received data: " << recvBuffer << std::endl;
+
+            result = send(ClientSocket, sendBuffer, (int)strlen(sendBuffer), 0);
+
+            if (result == SOCKET_ERROR) 
+            {
+                std::cout << "Failed to send data back" << std::endl;
+                closesocket(ClientSocket);
+                freeaddrinfo(addResult);
+                WSACleanup();
+                return 1;
+            }
+        }
+        else if (result == 0) 
+        {
+            std::cout << "Connection closing..." << std::endl;
+        }
+        else 
+        {
+            std::cout << "recv failed with error " << WSAGetLastError() << std::endl;
+            closesocket(ClientSocket);
+            freeaddrinfo(addResult);
+            WSACleanup();
+            return 1;
+        }
+    } 
+    while (result > 0);
+
+    result = shutdown(ClientSocket, SD_SEND);
+
+    if (result == SOCKET_ERROR) 
+    {
+        closesocket(ClientSocket);
+        freeaddrinfo(addResult);
+        WSACleanup();
+        return 1;
+    }
+
+    closesocket(ClientSocket);
+    freeaddrinfo(addResult);
+    WSACleanup();
+    return 0;
+}
